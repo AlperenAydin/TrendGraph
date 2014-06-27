c0dda555d9fe0c03f5a80bdfadc2ae341c925305
Alpy94 <warrior.1994@hotmail.com>
2014-06-27 14:05:58 +0300

    Added the flag for the information

--- Main.hs
+++ Main.hs
@@ -6,11 +6,13 @@ import Trend
 import Date
 import FromFiles
 
-import Diagrams.Prelude
+import Diagrams.Prelude hiding ((<>))
 import Diagrams.Backend.Cairo.CmdLine
+import Diagrams.Backend.CmdLine
 import Diagrams.TwoD.Shapes
 import Diagrams.TwoD.Text
 import Diagrams.TrailLike
+import Options.Applicative
 
 import qualified Data.Map as Map
 import Data.Time.Clock
@@ -22,20 +24,34 @@ import Control.Monad.Reader
 import System.IO
 import System.Environment
 
+
+-- Source file flag:
+
+data Source = Source String
+
+instance Parseable Source where
+  parser = Source
+           <$> strOption ( long "Source"
+                           <> metavar "FILE"
+                           <> help "File that has the info")
+
 points t = map (\ n ->(addUTCTime (n*d) t,timeToDouble  n^2)) [0, 1.0 .. 10]
 
 cross = hrule 1 <> vrule 1
 
 setting = Map.empty
 
-main = do putStrLn " Type the file name :"
-          file <- getLine
-          timelist <- openFile file ReadMode
-          times <- hGetContents timelist
-          timenum <- return $ timeListFromString times
-          case runEnv setting (graph timenum) of
-            (Right s) -> defaultMain( s <> circle 0.1 # fc red :: Diagram B R2)
-            (Left s) -> print s
+graphfromFile src = do file <- openFile src ReadMode
+                       times <- hGetContents file
+                       timenum <- return $ timeListFromString times
+                       case runEnv setting ( graph timenum) of
+                         (Right s) -> return $ (s <> circle 0.1 # fc red)
+                         (Left st) -> return $ (circle 1 # fc red)
+                         
+main = mainWith ( \ (Source src) -> graphfromFile src)
+
+
+  
 {-timelist <- openFile "TestList.txt" ReadMode
           times <- hGetContents timelist
           timenum <- return $ timeListFromString times-}
